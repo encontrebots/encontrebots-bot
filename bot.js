a711a93a2bbd79264e3f55a68e15cd14c1356e7f
yADGithub <2007andredoe@gmail.com>
2022-02-05 11:42:59 -0300

    Primeiras configurações

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
 require('dotenv').config();
+const config = require('./config');
+const {
+    Client,
+    Collection
+} = require('eris');
+const client = new Client(config.token, {
+    restMode: true,
+    intents: ['all']
+});
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.connect();
---
+++ config/config.example.js
@@ -0,0 +1,5 @@
+module.exports = {
+    prefix: 'bdd',
+    token: process.env.TOKEN,
+    mongo: process.env.MONGO
+}
