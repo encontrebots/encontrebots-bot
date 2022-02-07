de2360d7d23c4660ba1f3ec067071e26ffbc14f7
Leonardo <70207858+Leonardo-ST@users.noreply.github.com>
2022-02-07 17:27:02 -0300

    Embed

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,6 +1,18 @@
 
 exports.run = async (client, message, args) => {
 
+const EmbedBuilder = require('../../utils/EmbedBuilder').Embed;
+			
+			const embed = new EmbedBuilder();
+			embed.setTitle();
+			embed.setColor('#3498DB');
+			embed.setDescription(``);
+			embed.setFooter("Adg é gay")
+			message.channel.createMessage({
+				content: message.author.mention,
+				embeds: [embed.get()]
+			});
+
 };
 
 exports.help = {
