18ccafb32a71c5c54e22c06e6aa465045329f2e7
Leonardo <70207858+Leonardo-ST@users.noreply.github.com>
2022-02-07 12:42:36 -0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,1 +1,11 @@
 
+exports.run = async (client, message, args) => {
+
+};
+
+exports.help = {
+	name: 'help',
+	aliases: ['ajuda'],
+	category: 'info',
+	description: 'Mostra os comandos do bot!'
+};
